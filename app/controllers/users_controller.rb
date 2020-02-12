83e717ea314f0788bf1e9156cfefaaf380ee9d0c
theMindStone <nelson.a.caudill@gmail.com>
2020-02-11 23:48:09 -0500

    Add users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.create(user_params)
+
+    redirect_to @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+                          :first_name,
+                          :last_name,
+                          :password,
+                          :password_confirmation,
+                          :gi_skill,
+                          :nogi_skill,
+                          :weight
+                        )
+  end
 end
