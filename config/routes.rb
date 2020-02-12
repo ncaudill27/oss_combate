16a50d64b7887eea82501c21d974f83e9f65c536
theMindStone <nelson.a.caudill@gmail.com>
2020-02-12 12:55:31 -0500

    Begin SessionsController with actions new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  
   resources :users, except: [:new, :index]
   resources :tournaments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
