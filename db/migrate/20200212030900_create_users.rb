6090402edbe74dce48de57a9501ea01ddefa18aa
theMindStone <nelson.a.caudill@gmail.com>
2020-02-11 22:43:12 -0500

    Begin UsersController with new action

---
+++ db/migrate/20200212030900_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :gi_skill
+      t.string :nogi_skill
+      t.float :weight
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_235957) do
+ActiveRecord::Schema.define(version: 2020_02_12_030900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 2020_02_11_235957) do
     t.datetime "date"
     t.datetime "registration_open"
     t.datetime "registration_close"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "gi_skill"
+    t.string "nogi_skill"
+    t.float "weight"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
