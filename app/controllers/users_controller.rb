32aa2af9e9216860880444378a09e5a58e25b4f8
theMindStone <nelson.a.caudill@gmail.com>
2020-02-12 00:18:53 -0500

    DRY UsersController and add destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:new, :show, :edit, :update, :destroy]
   def new
-    @user = User.new
   end
 
   def create
@@ -11,14 +10,32 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_id(params[:id])
   end
 
   def edit
-    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+    @user.update(user_params)
+
+    redirect_to @user
+  end
+
+  def destroy
+    @user.destroy
+
+    redirect_to tournaments_path
   end
 
   private
+
+  def set_user
+    if params[:id]
+      @user = User.find_by_id(params[:id])
+    else
+      @user = User.new
+    end
+  end
 
   def user_params
     params.require(:user).permit(
