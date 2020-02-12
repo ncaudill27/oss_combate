612968e6264b664e48b80d97ab10a7e73cfb669b
theMindStone <nelson.a.caudill@gmail.com>
2020-02-11 20:29:22 -0500

    Create tournaments/new action and form

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -7,4 +7,18 @@ class TournamentsController < ApplicationController
   def show
     @tournament = Tournament.find_by_id(params[:id])
   end
+
+  def new
+    @tournament = Tournament.new
+  end
+
+  def create
+    
+  end
+  
+  private
+
+  def tournament_params
+    params.require(:tournament).permit(:organization, :url, :date, :registration_open, :registration_close)
+  end
 end
