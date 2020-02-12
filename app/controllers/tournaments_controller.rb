33866a6b7885dca97b6592e434f243014bf5e8da
theMindStone <nelson.a.caudill@gmail.com>
2020-02-11 19:25:56 -0500

    Tournaments show route and view

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -3,5 +3,8 @@ class TournamentsController < ApplicationController
   def index
     @tournaments = Tournament.all
   end
-  
+
+  def show
+    @tournament = Tournament.find_by_id(params[:id])
+  end
 end
