1d3462a4cf30a5fa2a14823c53edeb738cb4241d
theMindStone <nelson.a.caudill@gmail.com>
2020-02-12 14:24:26 -0500

    Add EventsController and begin to join Users/Tournaments

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,12 @@
+class EventsController < ApplicationController
+
+  def create
+    byebug
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -4,5 +4,6 @@ class Tournament < ApplicationRecord
   has_many :users, through: :events
 end
 
+#! Add location attribute
 #? Would creating instance methods here or in Division model
 #? allow fewer queries?
